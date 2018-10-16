13e35c27a10e20cff8a964240e8c1e75e40c11cd
NikQ <780366011@qq.com>
2018-10-16 22:46:51 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,38 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import axios from 'axios'
+import Goods from '@/components/goods/Goods'
+import Ratings from '@/components/ratings/Ratings'
+import Seller from '@/components/seller/Seller'
+
+Vue.config.productionTip = false
+
+Vue.prototype.$http = axios
+
+Vue.use(VueRouter)
+
+const routes = [
+  {path:'/',redirect:'/goods'},
+  {path:'/goods',component:Goods},
+  {path:'/ratings',component:Ratings},
+  {path:'/seller',component:Seller},
+  {path:'/*',redirect:'/goods'}
+]
+
+const router = new VueRouter({
+  routes,
+  // 默认生成选中状态的样式
+  linkActiveClass:'active',
+  mode:'history'
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
